cbd5fda63d4de9e442e10c0f171becea3e3f8f01
Brad <brad.solomon.1124@gmail.com>
2017-11-02 22:40:59 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -14,27 +14,29 @@ with _open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pyfinance',
-    description='Python package designed for general financial and security returns analysis.',
+    description='Python package designed for general financial'
+                ' and security returns analysis.',
     long_description=long_description,
-    version='0.1.0',
+    version='0.1.3',
     author='Brad Solomon',
     author_email='brad.solomon.1124@gmail.com',
     url='https://github.com/bsolomon1124/pyfinance',
+    download_url='https://github.com/bsolomon/pyfinance/archive/0.1.tar.gz',
     license='MIT',
     install_requires=[
-        'bs4 >= 4.6.0',
-        'Matplotlib >= 1.1',
-        'Numpy >= 0.7.0',
+        'beautifulsoup4 >= 4.6.0',
+        'matplotlib >= 1.1',
+        'numpy >= 0.7.0',
         'pandas_datareader >= 0.5.0',
-        'Pandas >= 0.20.1',
+        'pandas >= 0.20.1',
         'requests >= 2.11.1',
         'scipy >= 0.10.0',
         'seaborn >= 0.8.0',
         'selenium >= 3.5.0',
-        'sklearn >= 0.18.0',
+        'scikit-learn >= 0.18',
         'statsmodels >= 0.6.0',
         'xlrd >= 0.5.4',
-        'xmltodict >= 0.10.0'        
+        'xmltodict >= 0.10.0'
         ],
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -48,11 +50,11 @@ setup(
         ],
     keywords=[
         'finance',
-        'investment', 
-        'analysis', 
-        'regression', 
+        'investment',
+        'analysis',
+        'regression',
         'options',
-        'securities', 
+        'securities',
         'CAPM',
         'machine learning',
         'risk'
